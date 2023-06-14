acbebccca09a187e8d4e7cad24293e5acbdf4c3e
Skarcito <ddvloayza@gmail.com>
2023-06-14 01:51:52 -0500

    Cahnges

--- ghmingenieros/src/apps/core/urls.py
+++ ghmingenieros/src/apps/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from apps.core.views import Error404View
 
-from .views import AboutView, HomeView, MisionView, ServicesView, ContactView
+from .views import AboutView, CapacitacionView, ElaboracionView, EscuelaView, HomeView, MetalografiaView, MisionView, PruebaView, ServicesView, ContactView, EnsayosView, TratamientoView, VerificacionView
 
 app_name = "core"
 
@@ -11,6 +11,14 @@ urlpatterns = [
     path("about/", AboutView.as_view(), name="about"),
     path("mision/", MisionView.as_view(), name="mision"),
     path("services/", ServicesView.as_view(), name="services"),
+    path("ensayos_no_destructivos/", EnsayosView.as_view(), name="ensayos-no-destructivos"),
+    path("capacitacion_y_certificacion_ndt_aws/", CapacitacionView.as_view(), name="capacitacion-y-certificacion-ndt-aws"),
+    path("elaboracion_y_calificacion_procedimiento/", ElaboracionView.as_view(), name="elaboracion-y-calificacion-procedimiento"),
+    path("escuela_soldadura/", EscuelaView.as_view(), name="escuela-soldadura"),
+    path("metalografia/", MetalografiaView.as_view(), name="metalografia"),
+    path("prueba_de_vacio_soldaduras/", PruebaView.as_view(), name="prueba-de-vacio-soldaduras"),
+    path("tratamiento_termico_dureza/", TratamientoView.as_view(), name="tratamiento-termico-dureza"),
+    path("verificacion_de_materiales/", VerificacionView.as_view(), name="verificacion-de-materiales"),
     path("contact/", ContactView.as_view(), name="contact"),
     path("404/", Error404View.as_view(), name="error-404"),
 ]
--- ghmingenieros/src/apps/core/views.py
+++ ghmingenieros/src/apps/core/views.py
@@ -25,6 +25,33 @@ class ServicesView(TemplateView):
     template_name = "core/services.html"
 
 
+class EnsayosView(TemplateView):
+    template_name = "core/services/ensayos-no-destructivas.html"
+
+
+class CapacitacionView(TemplateView):
+    template_name = "core/services/capacitacion-y-certificacion-ndt-aws.html"
+
+class ElaboracionView(TemplateView):
+    template_name = "core/services/elaboracion-y-calificacion-procedimiento.html"
+
+class EscuelaView(TemplateView):
+    template_name = "core/services/escuela-soldadura.html"
+
+class MetalografiaView(TemplateView):
+    template_name = "core/services/metalografia.html"
+
+class PruebaView(TemplateView):
+    template_name = "core/services/prueba-de-vacio-soldaduras.html"
+
+class TratamientoView(TemplateView):
+    template_name = "core/services/tratamiento-termico-dureza.html"
+
+
+class VerificacionView(TemplateView):
+    template_name = "core/services/verificacion-de-materiales.html"
+
+
 class ContactView(TemplateView):
     template_name = "core/contact.html"
 
